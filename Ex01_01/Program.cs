5de0e64162ac28f73975b1b2576d15ff4f4c4a9a
Yoed Halberstam <yoedha@mta.ac.il>
2025-04-09 12:23:34 +0300

    another check

--- Ex01_01/Program.cs
+++ Ex01_01/Program.cs
@@ -10,18 +10,7 @@ namespace Ex01_01
     {
         static void Main(string[] args)
         {
-            // Declare a variable to hold the user's name
-            string name;
-
-            // Prompt the user for their name
-            Console.Write("Please enter your name: ");
-            name = Console.ReadLine();
-
-            // Display a greeting message
-            Console.WriteLine($"Hello, {name}!");
-
-            // Wait for user input before closing the console window
-            Console.ReadKey();
+          
         }
     }
 }
